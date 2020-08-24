cc08ab4d0a25685cc3bea3ec270896fe359c3963
Karim Alomari <karim.alomari@gmail.com>
2020-08-24 15:01:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,32 @@ class Song
     end
   end
   
-  
+  def self.alphabetical
+    @@all.sort_by{|value| value.name}
+  end
+
+  def self.new_from_filename(filename)
+    song = Song.new
+    seperate = filename.split("-")
+    seperate_song = seperate[1].split(".")
+    song.artist_name = seperate[0].strip
+    song.name = seperate_song[0].strip
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = Song.new
+    @@all << song
+    seperate = filename.split("-")
+    seperate_song = seperate[1].split(".")
+    song.artist_name = seperate[0].strip
+    song.name = seperate_song[0].strip
+    song
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
 
   def save
     self.class.all << self
